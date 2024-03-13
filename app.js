29b10667b1fd906fd8ac0dd7cc219ef359aeabba
godricvasa <112002007+godricvasa@users.noreply.github.com>
2024-03-13 21:08:47 +0530

    Update app.js

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
 // Connect to MongoDB Atlas
-const uri = "mongodb+srv://godricvasa:N8jgwBGQHhbBfBUs@cluster0.sotnlv5.mongodb.net/";
+const uri = "mongodb+srv://godricvasa:N8jgwBGQHhbBfBUs@cluster0.sotnlv5.mongodb.net/test";
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true
