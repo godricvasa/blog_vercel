4bb5dd744d3795a1c7c8d4e21641cd48db56f3e3
godricvasa <112002007+godricvasa@users.noreply.github.com>
2024-03-14 05:45:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const app = express();
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
-const homeStartingContent ="hello";
 
 // Connect to MongoDB Atlas
 const uri = "mongodb+srv://godricvasa:7IHLhNUBSeeEkeFZ@blogify.q5vcavh.mongodb.net/?retryWrites=true&w=majority&appName=blogify";
@@ -26,17 +25,16 @@ const PostColl = mongoose.model("PostColl", postSchema);
 app.get("/", async function(req, res) {
     const ele = await PostColl.find();
     res.render("home", {
-        startContent: homeStartingContent,
         posts: ele
     });
 });
 
 app.get("/about", function(req, res) {
-    res.render("about", {startContent: aboutContent});
+    res.render("about", {startContent: "I am vasa"});
 });
 
 app.get("/contact", function(req, res) {
-    res.render("contact", {startContent: contactContent});
+    res.render("contact", {startContent: "godricvasa@gmail.com"});
 });
 
 app.get("/compose", function(req, res) {
@@ -61,12 +59,6 @@ app.get('/posts/:postid', async function(req, res) {
         title: selectedPost.title,
         content: selectedPost.content
     });
-});
-app.get("/about", function(req, res) {
-    res.render("about");
-});
-app.get("/contact", function(req, res) {
-    res.render("contact");
 });
 app.listen(port, function() {
     console.log(`Server started on port ${port}`);
